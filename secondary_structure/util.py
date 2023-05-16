bac7b088179d78cbb64ff9ee33bf77a5a5603d72
AlbericDeLajarte <albericlajarte@gmail.com>
2023-05-15 17:00:24 -0700

    adapted import_structure for pairing matrix

--- secondary_structure/util.py
+++ secondary_structure/util.py
@@ -28,35 +28,37 @@ seq2int = {
 dot2int = {'.': 1, '(': 2, ')': 3, 'X': 0}
 int2dot = ['X', '.', '(', ')']
 
-def import_structure(path_to_structures=None, data_type = 'full', size=None, save=False, reload=True):
+def import_structure(path_to_structures=None, size=None, save=False, reload=True):
     """
-    Import the secondary structure dataset and convert to integer encoding, with padding.
+    Import the secondary structure dataset and convert to pairing matrix, with padding.
+    Each pairing matrix is directly saved as a separate npy file.
 
     Each row of the dataset contains a sequence and a structure.
-    The sequences contains the nucleotides A, C, G, T, U, Y, R, K, W, S, M, B, D, H, V, N
-    The structures is a dot-bracket notation e.g. (((.)))
+    The sequences contains the nucleotides A, C, G, T, U, N
+    The structures is binary pairing matrix with 0 for unpaired bases and 1 for paired bases
 
     :param path_to_structures: Path to the secondary structure dataset in json format
     :param size: The number of datapoints to import
     :param save: Whether to save the dataset as a numpy array
     :param reload: Whether to reload the dataset from the numpy array
-    :param test: Whether to import the test dataset
 
-    :return: A tuple with the integer encoded sequences and structures in numpy arrays
+    :return: The sequences as numpy array and the list of numpy file names of the pairing matrices
     """
-
-    assert data_type in ['full', 'test', 'train', 'binary', 'dot_bracket'], "Type must be 'full', 'test', 'binary', or 'train' or 'dot_bracket'"
 
     # Paths to the dataset
     dirname = os.path.dirname(os.path.abspath(__file__))
-    save_path = [os.path.join(dirname, 'dataset', data_type, 'processed_sequences.npy'),
-                 os.path.join(dirname, 'dataset', data_type, 'processed_structures.npy')]
+    save_path = [os.path.join(dirname, 'dataset', 'pairing_matrix', 'processed_sequences.npy'),
+                 os.path.join(dirname, 'dataset', 'pairing_matrix', 'processed_structures'),
+                 os.path.join(dirname, 'dataset', 'pairing_matrix', 'processed_structures', 'references.txt')]
+    
+    if not os.path.exists(save_path[1]):
+        os.makedirs(save_path[1])
 
     if path_to_structures is None:
-        path_to_structures = os.path.join(dirname, 'dataset', data_type, 'secondary_structure.json')
+        path_to_structures = os.path.join(dirname, 'dataset', 'pairing_matrix', 'secondary_structure_10k.json')
 
     # Import the dataset and check size
-    df = pd.read_json(path_to_structures)
+    df = pd.read_json(path_to_structures).T
     if size is None:
         size = len(df)
     elif size > len(df):
@@ -65,41 +67,41 @@ def import_structure(path_to_structures=None, data_type = 'full', size=None, sav
 
     # Check if the dataset is already saved
     if reload:
-        if os.path.exists(save_path[0]) and os.path.exists(save_path[1]):
+        if os.path.exists(save_path[0]) and os.path.exists(save_path[2]):
             print("Loading saved dataset")
             sequences = np.load(save_path[0])
-            structures = np.load(save_path[1])
+            references = np.loadtxt(save_path[2], dtype=str)
 
-            if len(sequences) < size or len(structures) < size:
+            if len(sequences) < size or len(references) < size:
                 print("Dataset too small, creating new one")
-                return import_structure(path_to_structures, data_type=data_type, size=size, save=save, reload=False)
+                return import_structure(path_to_structures, size=size, save=save, reload=False)
             else:
                 idx = np.random.choice(len(sequences), size=size, replace=False)
                 if save:
                     np.save(save_path[0], sequences[idx])
-                    np.save(save_path[1], structures[idx])
+                    np.save(save_path[2], references[idx])
                 
-                return sequences[idx], structures[idx]
+                return sequences[idx], references[idx]
         else:
             print("Dataset not found, creating new one")
-            return import_structure(path_to_structures, data_type=data_type, size=size, save=save, reload=False)
+            return import_structure(path_to_structures, size=size, save=save, reload=False)
         
 
     else:        
         
         # Get a random sample of the dataset
         idx = np.random.choice(len(df), size=size, replace=False)
-        df = df.iloc[idx].reset_index(drop=True)
+        df = df.iloc[idx]
 
         # Get max sequence length
         max_seq_len = df['sequence'].str.len().max()
 
-        # Init numpy arrays for sequences and structures
+        # Init numpy arrays for sequences
         sequences = np.zeros((len(df), max_seq_len))
-        structures = np.zeros((len(df), max_seq_len))
+        references = []
 
         # Iterate over the rows of the dataframe
-        for i, row in df.iterrows():
+        for i, (reference, row) in enumerate(df.iterrows()):
 
             # Print progress
             if i%1000:
@@ -108,29 +110,33 @@ def import_structure(path_to_structures=None, data_type = 'full', size=None, sav
             
             # Apply zero padding to each row (sequence and structure)
             row['sequence'] = row['sequence'].upper().ljust(max_seq_len, 'X')
-            row['structure'] = row['structure'].ljust(max_seq_len, 'X')
-            # row['structure'] = row['structure'] + [0]*(max_seq_len-len(row['structure']))
 
             # Integer encoding of the sequence, with padding as 'X' 
             for j, base in enumerate(row['sequence']):
                 sequences[i,j] = seq2int[base]
 
-            # One hot encoding of the structure, with padding as 'X' 
-            for j, struct in enumerate(row['structure']):
-                structures[i,j] = dot2int[struct]
-                # structures[i,j] = struct
+            # Create pairing matrix from list of paired bases
+            paired_bases = np.array(row['paired_bases'])
+            pairing_matrix = np.zeros((max_seq_len, max_seq_len)).astype(bool)
+            if len(paired_bases) > 0:
+                pairing_matrix[paired_bases[:,0], paired_bases[:,1]] = 1
+                pairing_matrix[paired_bases[:,1], paired_bases[:,0]] = 1
+
+            if save:
+                np.save(os.path.join(save_path[1], reference+'.npy'), pairing_matrix)
+                references.append(reference)
 
         # Save sequences and structures as numpy arrays
         if save:
             np.save(save_path[0], sequences)
-            np.save(save_path[1], structures)
+            np.savetxt(save_path[2], references, fmt='%s')
 
         
-        return sequences, structures
+        return sequences, references
 
 
 
 if __name__ == '__main__':
 
-    sequences, structures = import_structure(data_type='dot_bracket', save=True, reload=False)
-    print("Loaded full dataset with shape: \n", sequences.shape, "\n", structures.shape)
+    sequences, references = import_structure(save=True, reload=False)
+    print("Loaded full dataset with shape: \n", sequences.shape, "\n", len(references))
