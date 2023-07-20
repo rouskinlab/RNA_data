7e52443319a6d01a6f00dd5d321c802808b1d328
Yves Martin <59341242+yvesmartindestaillades@users.noreply.github.com>
2023-07-19 18:01:59 -0700

    env variables are part of pytest

--- rouskinhf/env.py
+++ rouskinhf/env.py
@@ -9,7 +9,6 @@ DATA_FOLDER_TESTING = os.environ['DATA_FOLDER_TESTING']
 RNASTRUCTURE_PATH = os.environ['RNASTRUCTURE_PATH']
 RNASTRUCTURE_TEMP_FOLDER = os.environ['RNASTRUCTURE_TEMP_FOLDER']
 
-
 for var in ["HUGGINGFACE_TOKEN", "DATA_FOLDER", "DATA_FOLDER_TESTING", "RNASTRUCTURE_PATH", "RNASTRUCTURE_TEMP_FOLDER"]:
     if len(os.environ[var]) == 0:
         raise ValueError(f"Environment variable {var} is not set. Please run `source env`.")
