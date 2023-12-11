257c3d42b3db6db0221cbee69c89b31e32595290
yvesmartindestaillades <yves.mrtn98@gmail.com>
2023-12-10 20:30:37 -0600

    add utils to rouskinhf root

--- rouskinhf/__init__.py
+++ rouskinhf/__init__.py
@@ -1,2 +1,9 @@
 from .conversion import convert
 from .hf import upload_dataset, download_dataset, get_dataset
+from .util import (
+    int2dot, 
+    dot2int,
+    int2seq,
+    seq2int,
+    UKN
+)
